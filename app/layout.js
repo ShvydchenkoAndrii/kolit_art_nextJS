843de5794792fe917f753a5f677a507c53cb7aec
unknown <shvydchenkoandriy@gmail.com>
2023-07-14 21:12:17 +0300

    added shortcut ico

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import Head from 'next/head';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Kolir.Art Studio',
   description: '',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/kolir_art_logo.jpg" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
